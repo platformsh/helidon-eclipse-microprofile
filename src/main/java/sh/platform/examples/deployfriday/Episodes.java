e1722ccca3aafa6095f1cff8a1cff2c77bed7d14
otaviojava <otaviopolianasantana@gmail.com>
2020-07-26 13:43:42 -0300

    updates eposidoes

--- src/main/java/sh/platform/examples/deployfriday/Episodes.java
+++ src/main/java/sh/platform/examples/deployfriday/Episodes.java
@@ -3,9 +3,11 @@ package sh.platform.examples.deployfriday;
 
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
+import javax.json.bind.annotation.JsonbVisibility;
 import java.util.List;
 
 @Schema(name = "Episodes", description = "The collection of episodes")
+@JsonbVisibility(FieldVisibility.class)
 public class Episodes {
 
     @Schema(required = true)
