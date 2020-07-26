863de0ba0646a2ecc2c4d04ec96f0a0f418c3b55
otaviojava <otaviopolianasantana@gmail.com>
2020-07-26 13:43:17 -0300

    updates episody repository

--- src/main/java/sh/platform/examples/deployfriday/EpisodeRepository.java
+++ src/main/java/sh/platform/examples/deployfriday/EpisodeRepository.java
@@ -3,7 +3,10 @@ package sh.platform.examples.deployfriday;
 import jakarta.nosql.mapping.Repository;
 
 import javax.enterprise.context.ApplicationScoped;
+import java.util.List;
 
 @ApplicationScoped
 public interface EpisodeRepository extends Repository<Episode, Long> {
+
+    List<Episode> findAll();
 }
